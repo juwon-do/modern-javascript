f45722491bab6396e52495a1617532a5648f4126
Juwon Do <nobeldr13@gmail.com>
2023-01-09 12:54:17 -0600

    install webpack and nerdamer js library

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
-console.log('Hello from js');
-setTimeout(function() {
-  console.log('First task done!');
-  setTimeout(function() {
-    console.log('Second task done!');
-    setTimeout(function() {
-      console.log('Third task done!');
-      setTimeout(function() {
-        console.log('Fourth task done!');
-      }, 2000);
-    }, 2000);
-  }, 2000);
-}, 2000);
+/* global moment dayjs */
+const nerdamer = require("nerdamer/all.min");
+var moment = require('moment'); // require
+moment().format(); 
 
+console.log('Hello from js');
+// setTimeout(function() {
+//   console.log('First task done!');
+//   setTimeout(function() {
+//     console.log('Second task done!');
+//     setTimeout(function() {
+//       console.log('Third task done!');
+//       setTimeout(function() {
+//         console.log('Fourth task done!');
+//       }, 2000);
+//     }, 2000);
+//   }, 2000);
+// }, 2000);
+
+console.log(moment().format("MMM Do YY"));
+
+var e = nerdamer('x^2+2*(cos(x)+x*x)');
+console.log(e.text());
